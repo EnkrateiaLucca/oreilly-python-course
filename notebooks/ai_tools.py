ffde885f99f8a2a0b8896c49bb9a5bf607ec88bc
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2025-02-02 17:20:00 +0000

    added extraction examples

--- notebooks/ai_tools.py
+++ notebooks/ai_tools.py
@@ -1,27 +1,63 @@
 from openai import OpenAI
+import anthropic
+import ollama
 
-client = OpenAI()
-
-def ask_ai(prompt):
+def ask_ai(prompt, model_name="gpt-4o-mini"):
     """
     Send prompt to an LLM and get output text back.    
     """
-    
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a helpful programming assistant."
-            },
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        temperature=0.2
-    )
-    
-    return response.choices[0].message.content
+    if "claude" in model_name:
+        client = anthropic.Anthropic()
+
+        # Send a message to the Claude AI
+        response = client.messages.create(
+            model=model_name,
+            messages=[
+                {
+                    "role": "user", "content": prompt
+                }
+                ],
+            max_tokens=4000,
+        )
+        output = response.content[0].text
+        return output        
+    else:
+        client = OpenAI()
+        response = client.chat.completions.create(
+            model=model_name,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant."
+                },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+        )
+        output = response.choices[0].message.content
+        return output
 
 
+def ask_local_ai(prompt, model_name="llama3.2", structured=False):
+    """
+    Send prompt to a local LLM and get output text back.
+    """
+    client = ollama.Client()
+    if structured:
+        response = client.chat(model=model_name, 
+                               messages=
+                               [
+                                   {"role": "user", "content": prompt}
+                                ],
+                               format='json')
+        return response.message.content
+    else:
+        response = client.chat(model=model_name, 
+                               messages=
+                               [
+                                   {"role": "user", "content": prompt}
+                                ])
+        return response.message.content
+        
